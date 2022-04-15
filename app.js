53a5c8af1144c00535f1ddcf4621a70da97d0903
unknown <mehrdadzare2003@gmail.com>
2022-04-15 14:22:02 +0430

    dser1

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const startMonitoring = async () => {
   // const revisionInfo = await browserFetcher.download('970485');
   const browser = await puppeteer.launch({
     headless: true,
-    executablePath: process.env.CHROMIUM_PATH,
+    executablePath: '/path/to/Chrome' ,
     args: [
       '--no-sandbox',
       '--disable-gpu',
